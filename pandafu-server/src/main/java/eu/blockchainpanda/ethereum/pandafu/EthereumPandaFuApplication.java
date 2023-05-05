8a4a5314c60f35d7e5ac904fe20111dc47a8bfae
Angela-Maria Despotopoulou <a.despotopoulou@gmail.com>
2023-05-05 17:07:59 +0300

    Initial commit of Spring project

---
+++ pandafu-server/src/main/java/eu/blockchainpanda/ethereum/pandafu/EthereumPandaFuApplication.java
@@ -0,0 +1,13 @@
+package eu.blockchainpanda.ethereum.pandafu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EthereumPandaFuApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EthereumPandaFuApplication.class, args);
+	}
+
+}
---
+++ pandafu-server/src/test/java/eu/blockchainpanda/ethereum/pandafu/EthereumPandaFuApplicationTests.java
@@ -0,0 +1,13 @@
+package eu.blockchainpanda.ethereum.pandafu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EthereumPandaFuApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
